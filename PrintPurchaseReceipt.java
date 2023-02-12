1ae805751820294fb4f8e8002d3a926e5a1489ee
mkedar <78320557+mkedar@users.noreply.github.com>
2023-02-11 23:42:52 -0800

    Add files via upload

---
+++ PrintPurchaseReceipt.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;  
+
+class PrintPurchaseReceipt {
+    // Matan Kedar
+    // 2.8.23 
+    public static void main(String[] args) {
+        // create the scanner object 
+        Scanner myObj = new Scanner(System.in);
+
+        // gather input 
+        System.out.println("Please enter the name of the item: ");
+        String itemName = myObj.nextLine();
+        System.out.println("Please enter the price for each item: ");
+        Double itemPrice = myObj.nextDouble();
+        System.out.println("Please enter the quantity: ");
+        int itemQuantity = myObj.nextInt();
+        System.out.println("Is this a gift? ");
+        String gift = myObj.next();
+        char giftC = gift.charAt(0);
+        System.out.println("Please enter the description of the occasion: ");
+        String description = myObj.next();
+        
+        // print inputs
+        double TAX = 0.1;
+        System.out.println("========== RECEIPT ==========");
+        System.out.println("Name:       " + itemName);
+        System.out.println("Price:      $" + itemPrice);
+        System.out.println("Quantity:   " + itemQuantity);
+        System.out.println("Subtotal:   $" + itemQuantity*itemPrice);
+        System.out.println("Tax:        $" + itemQuantity*itemPrice*TAX);
+        System.out.println("Total:      $" + ((itemQuantity*itemPrice) + itemQuantity*itemPrice*TAX));
+        System.out.println("Gift (y/n): " + giftC );
+        System.out.println("Occasion:   " + description);
+        System.out.println("============================");
+    }
+}
---
+++ lab1.java
@@ -0,0 +1,32 @@
+class lab1 {
+    // Matan Kedar
+    // 2.1.23 
+    public static void main(String[] args) {
+        // Prints out the triangle 
+        System.out.println("Triangle:        "); 
+        System.out.println("       *"); 
+        System.out.println("      ***"); 
+        System.out.println("     *****"); 
+        System.out.println("    *******"); 
+        System.out.println("   *********\n"); 
+
+        // Prints out the rectangle  
+        System.out.println("Rectangle:        "); 
+        System.out.println("   ##########"); 
+        System.out.println("   #        #"); 
+        System.out.println("   #        #"); 
+        System.out.println("   #        #"); 
+        System.out.println("   ##########\n"); 
+        
+        // Prints out the diamond 
+        System.out.println("Diamond:        "); 
+        System.out.println("     %"); 
+        System.out.println("   %   %"); 
+        System.out.println("  %     %"); 
+        System.out.println("   %   %"); 
+        System.out.println("     %"); 
+
+
+
+    }
+}
