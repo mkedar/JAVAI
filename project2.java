db8f7ed86ea1a77cd468e94ced74de818fe45137
mkedar <78320557+mkedar@users.noreply.github.com>
2023-04-19 19:09:06 -0700

    Add files via upload

---
+++ project2.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+// Matan Kedar
+// 4/18/23
+
+class Assignment { // creating assignment object
+    private String variableName;
+    private int value;
+
+    public Assignment(String variableName, int value) {
+        this.variableName = variableName;
+        this.value = value;
+    }
+    public String getVariableName() {
+        return variableName;
+    }
+    public int getValue() {
+        return value;
+    }
+    @Override 
+    public String toString() {
+        return variableName + " = " + value;
+    }
+}
+
+public class project2 { 
+    public static String input(){ // gather input from user
+        Scanner myObj = new Scanner(System.in);  
+        boolean loop = true;
+	String input = ""; 
+    while(loop){  // filter input to be 1/4 words
+	    System.out.println("Please enter a command (add, list, stats or exit):");
+	    input = myObj.nextLine();
+ 	    if(input.equals("add") == true ||input.equals("list") == true||input.equals("stats") == true||input.equals("exit") == true){
+            loop = false;
+        }
+	    else{
+	        System.out.println("Unsupported command. Please try again.");  
+        }
+	}
+        return input;
+    }
+    public static boolean isNum(String input) {  // find if input is between 0-9 or has a negative sign
+        return input.matches("-?\\d+") ? true : false;
+    }
+    public static boolean isNum(char input) { // find if the char is a number
+        return Character.isDigit(input);
+    }    
+    public static Assignment add(){ // add a assignment to the vector 
+        Scanner myObj = new Scanner(System.in);
+        boolean loop = true;
+        String[] arr = null; 
+        while (loop) {
+            String input = myObj.nextLine().replaceAll("\\s", "");
+            try { // filter certian ways to create the assignments. And output the correct errors, when the errors are made.
+                arr = input.split("=", 2);
+                if (arr.length != 2 ){
+                    throw new Exception("Invalid assignment operator");
+                }
+                if (isNum(arr[0]) || isNum(arr[0].charAt(0))){
+                    throw new Exception("Invalid variable name.");
+                }
+                if (!isNum(arr[1])){
+                    throw new Exception("The assignment value is not a number.");
+                }
+                loop = false;
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        Assignment output = new Assignment(arr[0], Integer.parseInt(arr[1])); // combine the info from the for loop 
+        return output;
+    }    
+    public static void list(Vector<Assignment> vector){  // list all of the assignments in the vector
+        if(vector.size() == 0){ // check if vector is empty to display empty message
+            System.out.println( "There is no assignment");
+        }
+        else{
+            System.out.println("All assignments:"); // print all of the assignments if there are more than 0
+            for (int i = 0; i < vector.size(); i++) {
+                Assignment element = vector.get(i);
+                System.out.println(element);
+            }
+        }
+    }
+    public static void stats(Vector<Assignment> vector) { //calculate and print the stats 
+        if (vector.isEmpty()) {
+            System.out.println("No assignments found.");
+            return;
+        }
+        int pos = 0, neg = 0, counter = 0;
+        Assignment highest = vector.get(0);
+        Assignment smallest = vector.get(0);
+    
+        for (int i = 0; i < vector.size(); i++) { // calculate the smallest value, largest values, total postive/negative values and total values using a for loop
+            Assignment currentAssignment = vector.get(i);
+            if (currentAssignment.getValue() > highest.getValue()) {
+                highest = currentAssignment;
+            }
+            if (currentAssignment.getValue() < smallest.getValue()) {
+                smallest = currentAssignment;
+            }
+            if (currentAssignment.getValue() > 0) {
+                pos++;
+            } 
+            if (currentAssignment.getValue() < 0) {
+                neg++;
+            }
+            counter++;
+        }
+        System.out.println("Total number of assignments:          " + counter); // print out all of the calculated values
+        System.out.println("Total number of positive assignments: " + pos);
+        System.out.println("Total number of negative assignments: " + neg);
+        System.out.println("The largest assignment:               " + highest);
+        System.out.println("The smallest assignment:              " + smallest);
+        
+    }
+    public static void main(String[] args) { // main method 
+    System.out.println("Welcome to assignment recording program.");
+    Vector<Assignment> vector= new Vector<>(); // main vector creation to track all the assignments. 
+    while(true){ // while loop for the program to be able to be run indefiently 
+        String userInput = input(); // gather validated user input 
+        if(userInput.equals("add")){
+            Assignment assignment = add();
+            vector.add(assignment);
+        }
+        if(userInput.equals("list")){
+            list(vector);
+        }
+        if(userInput.equals("stats")){
+            stats(vector);
+        }
+        if(userInput.equals("exit")){
+                System.out.println("Thank you. Goodbye.");
+                System.exit(0); // exit program without an error
+        }
+        }
+    }
+}
+
